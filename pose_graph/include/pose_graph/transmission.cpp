6858e64c4b8325ee90f1b3c24025aeb2870ce9ea
Anagh Benjwal <benjwal.anagh@gmail.com>
2023-07-18 18:09:02 +0530

    transmission.cpp

--- pose_graph/include/pose_graph/transmission.cpp
+++ pose_graph/include/pose_graph/transmission.cpp
@@ -51,8 +51,14 @@ public:
         }
         convertCvMatToArray(keyframe.image, imgarray);
         
-        // float** KeypointArray = new float*[];
+        vio_T_w_i = keyframe.vio_T_w_i; 
+        vio_R_w_i = keyframe.vio_R_w_i;
+        T_w_i = keyframe.T_w_i;
+        R_w_i = keyframe.R_w_i;
+        origin_vio_T = keyframe.origin_vio_T;
+        origin_vio_R = keyframe.origin_vio_R;
 
+        // float** KeypointArray = new float*[];
         point3darr = new float[keyframe.point_3d.size() * 3];
         point2DuvArr = new float[keyframe.point_2d_uv.size() * 2];
         point2DnormArr = new float[keyframe.point_2d_norm.size() * 2];
@@ -77,14 +83,16 @@ public:
         convertCvKeypointsToArray(keyframe.window_keypoints, KeypointWindowArray);
         
         //brief desciptor conversion
-        // brief_descriptors = keyframe.brief_descriptors;
-        // window_brief_descriptors = keyframe.window_brief_descriptors;
+        brief_descriptors = keyframe.brief_descriptors;
+        window_brief_descriptors = keyframe.window_brief_descriptors;
 
         has_fast_point = keyframe.has_fast_point;
         has_loop = keyframe.has_loop;
         loop_index = keyframe.loop_index;
         loop_info = keyframe.loop_info;
     }
+
+
 
     double time_stamp; 
 	int index;
@@ -128,19 +136,19 @@ public:
     KeypointArrayGeneral* KeypointNormArray; //to replace vector<cv::KeyPoint> keypoints_norm;
     KeypointArrayGeneral* KeypointWindowArray; //to replace vector<cv::KeyPoint> window_keypoints;
 
-	// vector<BRIEF::bitset> brief_descriptors;
-	// vector<BRIEF::bitset> window_brief_descriptors;
+	vector<BRIEF::bitset> brief_descriptors;
+	vector<BRIEF::bitset> window_brief_descriptors;
     
 	bool has_fast_point;
 	bool has_loop;
 	int loop_index;
 	Eigen::Matrix<double, 8, 1> loop_info;
 
-private :
     int image_rows;
     int image_cols;
     int image_chans;
 
+private :
     void convertCvMatToArray(const cv::Mat& image, int***& imgarray){
         for (int i = 0; i < image.rows; i++){
             for (int j = 0; j < image.cols; j++){
@@ -167,8 +175,6 @@ private :
         }
     }
 
-
-
     void convertCvKeypointsToArray(const std::vector<cv::KeyPoint>& keypoints, KeypointArrayGeneral*& keypointarr){
         // for (size_t i = 0; i < keypoints.size(); ++i) {
         //     keypointArray[i][0] = static_cast<int>(keypoints[i].pt.x);
@@ -184,5 +190,61 @@ private :
             keypointarr[i].class_id = keypoints[i].class_id;
         }
 
+    }
+
+
+    /*
+    Converting TransmitKeyFrame back to KeyFrame object
+    */
+
+    cv::Mat convertArrayToCvMat(int*** imgarray, int rows, int cols, int chans) {
+        cv::Mat image(rows, cols, CV_8UC(chans));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                cv::Vec3b pixel;
+                for (int k = 0; k < chans; k++) {
+                    pixel[k] = imgarray[i][j][k];
+                }
+                image.at<cv::Vec3b>(i, j) = pixel;
+            }
+        }
+
+        return image;
+    }
+
+    
+    std::vector<cv::Point3f> convertArrayToPoint3f(float* arr, int size) {
+        std::vector<cv::Point3f> points;
+
+        for (int i = 0; i < size; i += 3) {
+            cv::Point3f point(arr[i], arr[i + 1], arr[i + 2]);
+            points.push_back(point);
+        }
+
+        return points;
+    }
+
+    // Convert float array to vector<cv::Point2f>
+    std::vector<cv::Point2f> convertArrayToPoint2f(float* arr, int size) {
+        std::vector<cv::Point2f> points;
+
+        for (int i = 0; i < size; i += 2) {
+            cv::Point2f point(arr[i], arr[i + 1]);
+            points.push_back(point);
+        }
+
+        return points;
+    }
+
+    // Convert KeypointArrayGeneral array to vector<cv::KeyPoint>
+    std::vector<cv::KeyPoint> convertArrayToCvKeypoints(TransmitKeyFrame::KeypointArrayGeneral* keypointarr, int size) {
+        std::vector<cv::KeyPoint> keypoints;
+
+        for (int i = 0; i < size; i++) {
+            cv::KeyPoint keypoint(keypointarr[i].x, keypointarr[i].y, keypointarr[i].size, keypointarr[i].angle, keypointarr[i].response, keypointarr[i].octave, keypointarr[i].class_id);
+            keypoints.push_back(keypoint);
+        }
+
+        return keypoints;
     }
 };
